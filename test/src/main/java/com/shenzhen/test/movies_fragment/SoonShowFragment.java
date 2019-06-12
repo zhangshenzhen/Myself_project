78b62949d3c0ee1f08159bd0e5f67ac1e32a7f10
zhangshenzhen <zhangshenzhen@pateo.com.cn>
2019-06-12 09:40:51 +0800

    测试提交

--- test/src/main/java/com/shenzhen/test/movies_fragment/SoonShowFragment.java
+++ test/src/main/java/com/shenzhen/test/movies_fragment/SoonShowFragment.java
@@ -1,29 +1,22 @@
 package com.shenzhen.test.movies_fragment;
 
 import android.view.View;
-import android.widget.TextView;
 
 import com.shenzhen.test.R;
-
-public class MovieOrderFrg extends BaseFragment {
-
-
+public class SoonShowFragment extends BaseFragment {
 
     @Override
     public int initLayout() {
-        return R.layout.fragment_movie_order;
+        return R.layout.fragment_movie_soon;
     }
-
 
     @Override
     public void beforeInitView() {
+
     }
 
     @Override
     public void initView() {
-
-        TextView tv_typle = view.findViewById(R.id.tv_typle);
-        tv_typle.setText("电影订单");
 
     }
 
